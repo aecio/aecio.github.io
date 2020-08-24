ac2b5e6535f3223b5c98ae9c8c967f415f11ade2
Aecio Santos <aecio.solando@gmail.com>
2020-08-24 14:37:56 -0400

    Add prepapre-deploy script

---
+++ prepare-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$(git status --porcelain)" ]; then
+  # Working directory is clean
+  sitegen build
+  SITEGEN_TMP_OUPUT=/tmp/sitegen-tmp-build
+  mkdir -p ${SITEGEN_TMP_OUPUT}
+  rm -rf ${SITEGEN_TMP_OUPUT}/*
+  cp src/CNAME ./output/
+  mv ./output/* ${SITEGEN_TMP_OUPUT}
+  git checkout master
+  git status
+  rm -r ./* &&  mv /tmp/sitegen-tmp-build/* .
+  git status
+  git add .
+  git status
+else
+  # Working directory has uncommitted changes
+  echo "Please make all changes are committed before running this script."
+fi
