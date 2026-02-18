179e1dc4d61444c30f4b86083fefd7e82e770a4e
Aécio Santos <aecio.solando@gmail.com>
2026-02-18 15:29:34 +0100

    Added shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+# To activate this environment, run:
+#   nix-shell shell.nix
+let
+  nixpkgs = import <nixpkgs> {};
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "site-env";
+    buildInputs = [
+      jdk21_headless
+    ];
+  }
